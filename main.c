b6992f0ffc02308b55687ad9fa1d6c4968767b81
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-08-30 01:42:37 -0300

    feat: criando a janela do jogo em tela cheia

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
+#include <allegro5/allegro.h>
 #include <stdio.h>
 
 int main()
 {
-  printf("...");
+  if (!al_init())
+  {
+    fprintf(stderr, "Falha ao inicializar Allegro.\n");
+
+    return 1;
+  }
+
+  ALLEGRO_MONITOR_INFO context;
+  al_get_monitor_info(0, &context);
+  int screen_width = context.x2 - context.x1;
+  int screen_height = context.y2 - context.y1;
+
+  ALLEGRO_DISPLAY *display = al_create_display(screen_width, screen_height);
+
+  if (!display)
+  {
+    fprintf(stderr, "Falha ao criar display.\n");
+
+    return 1;
+  }
+
+  while (true)
+  {
+  }
 
   return 0;
 }
