9c3891916cf47d5c5bd955fb3940b2fea9cf91f7
devpaulorcc <dev.paulorc@gmail.com>
2025-09-09 20:13:58 -0300

    refactor: melhores nomes para estados de telas e de menu

--- src/core/game.c
+++ src/core/game.c
@@ -1,3 +1,3 @@
 #include "game.h"
 
-GameState current_game_state = BOOTING;
+GameState current_game_state = GAME_MENU;
--- src/core/game.h
+++ src/core/game.h
@@ -1,13 +1,15 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <allegro5/allegro5.h>
+#include <stdbool.h>
+
 typedef enum
 {
-    BOOTING,
     GAME_MENU,
     GAME_CONFIG,
-    GAME_PLAY,
-    GAME_PAUSE,
+    GAME_PLAYING,
+    GAME_PAUSED,
     GAME_OVER
 } GameState;
 
