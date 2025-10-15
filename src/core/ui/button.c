4cc87798007a1884dd56f615da131a6622d00b2d
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-15 13:46:58 -0300

    fix: as cores não estavam renderizando
    Agora as cores são consumidas de uma posição fixa na memória.

--- src/core/ui/button.c
+++ src/core/ui/button.c
@@ -1,22 +1,32 @@
 #include "button.h"
 
-void draw_button(Button *btn, int mouse_x, int mouse_y, bool mouse_clicked)
+void draw_button(Button *button)
 {
-    al_draw_filled_rectangle(btn->x, btn->y, btn->x + btn->width, btn->y + btn->height,
-                             btn->text.fill_color);
+    int mouse_x = get_mouse_x();
+    int mouse_y = get_mouse_y();
+    bool clicked = mouse_is_clicked();
 
-    al_draw_rectangle(btn->x, btn->y, btn->x + btn->width, btn->y + btn->height,
-                      btn->text.border_color, 2);
+    float button_right = button->x + button->width;
+    float button_bottom = button->y + button->height;
 
-    al_draw_text(btn->text.font, btn->text.color, btn->x + btn->width / 2,
-                 btn->y + (btn->height / 2) - 10, ALLEGRO_ALIGN_CENTER, btn->text.content);
+    al_draw_filled_rectangle(button->x, button->y, button_right, button_bottom,
+                             *button->fill_color);
 
-    bool is_intersecting_horizontal = mouse_x >= btn->x && mouse_x <= btn->x + btn->width;
-    bool is_intersecting_vertical = mouse_y >= btn->y && mouse_y <= btn->y + btn->height;
-
-    if (mouse_clicked && is_intersecting_horizontal && is_intersecting_vertical)
+    if (button->border.border_color)
     {
-        if (btn->on_click)
-            btn->on_click();
+        float thickness = (button->border.thickness > 0) ? button->border.thickness : 4.f;
+        al_draw_rectangle(button->x, button->y, button_right, button_bottom,
+                          *button->border.border_color, thickness);
     }
+
+    float text_x = button->x + button->width / 2;
+    float text_y = button->y + button->height / 2 - 10;
+    al_draw_text(button->text.font, *button->text.color, text_x, text_y, ALLEGRO_ALIGN_CENTER,
+                 button->text.content);
+
+    bool is_inside_x = mouse_x >= button->x && mouse_x <= button_right;
+    bool is_inside_y = mouse_y >= button->y && mouse_y <= button_bottom;
+
+    if (clicked && is_inside_x && is_inside_y && button->on_click)
+        button->on_click();
 }
--- src/core/ui/button.h
+++ src/core/ui/button.h
@@ -3,26 +3,35 @@
 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
 #include <stdbool.h>
+
+#include "src/core/input.h"
 
 typedef void (*OnClickCallback)(void);
 
 typedef struct
 {
     const char *content;
-    ALLEGRO_COLOR color;
-    ALLEGRO_COLOR fill_color;
-    ALLEGRO_COLOR border_color;
+    const ALLEGRO_COLOR *color;
     ALLEGRO_FONT *font;
 } ButtonText;
 
 typedef struct
 {
-    float x,y,width,height;
+    const ALLEGRO_COLOR *border_color;
+    float thickness;
+} ButtonBorder;
+
+typedef struct
+{
+    float x, y, width, height;
+    const ALLEGRO_COLOR *fill_color;
     ButtonText text;
+    ButtonBorder border;
     OnClickCallback on_click;
 } Button;
 
-void draw_button(Button *btn, int mouse_x, int mouse_y, bool mouse_clicked);
+void draw_button(Button *button);
 
 #endif
