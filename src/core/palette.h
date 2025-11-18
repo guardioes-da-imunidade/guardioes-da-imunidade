e557a75f2b999e54a07a9a85e964bbce98c5ff6e
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-11-18 15:35:37 -0300

    chore: removendo comentários

--- src/core/palette.h
+++ src/core/palette.h
@@ -3,12 +3,10 @@
 
 #include <allegro5/allegro_color.h>
 
-// Básicos
 extern ALLEGRO_COLOR COLOR_BLACK;
 extern ALLEGRO_COLOR COLOR_WHITE;
 extern ALLEGRO_COLOR COLOR_YELLOW;
 
-// UI
 extern ALLEGRO_COLOR COLOR_ENEMY_BUTTON;
 extern ALLEGRO_COLOR COLOR_DEFENDER_BUTTON;
 
